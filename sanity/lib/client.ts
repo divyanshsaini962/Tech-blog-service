5d21d6617d498cb8012f55145b28627b198d83e8
Divyanshu Saini <112510115+divyanshsaini962@users.noreply.github.com>
2024-04-27 00:34:38 +0530

    change a env

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,6 +1,11 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+// import { apiVersion, dataset, projectId, useCdn } from '../env'
+
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-04-21'
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+export const projectId =process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+export const useCdn = false
 
 export const client = createClient({
   apiVersion,
