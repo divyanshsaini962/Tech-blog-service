76a17a38a272a83e958c25c0c60956ba4c6a16e8
Divyanshu Saini <112510115+divyanshsaini962@users.noreply.github.com>
2024-04-28 00:42:38 +0530

    comment in env.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 // import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-04-21'
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-export const projectId =process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export const useCdn = false
 
 export const client = createClient({
