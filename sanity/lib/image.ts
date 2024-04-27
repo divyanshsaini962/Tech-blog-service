40170cb28aa42add2d56362e02de6400d7790a57
Divyanshu Saini <112510115+divyanshsaini962@users.noreply.github.com>
2024-04-28 00:53:31 +0530

    change a imgage.ts

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,7 +1,10 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import type { Image } from 'sanity'
 
-import { dataset, projectId } from '../env'
+// import { dataset, projectId } from '../env'
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-04-21'
 
 const imageBuilder = createImageUrlBuilder({
   projectId: projectId || '',
